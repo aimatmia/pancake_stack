2153dba7cecf53fb5d9afa8f0063842ec6944712
Mia <mobrien@stuy.edu>
2017-04-26 20:17:06 -0400

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,128 @@
+from display import *
+from matrix import *
+from draw import *
+from copy import deepcopy
+
+ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box', 'sphere', 'torus' ]
+
+def parse_file( fname, edges, transform, screen, color ):
+
+    f = open(fname)
+    lines = f.readlines()
+
+    I = new_matrix()
+    ident(I)
+    stack = [I]
+
+    def new_coord(is_flat=False):
+        matrix_mult(stack[-1], edges)
+        if is_flat:
+            draw_lines(edges, screen, color)
+        draw_polygons(edges, screen, color)
+        edges[:] = []
+        
+    step = 0.1
+    c = 0
+
+    while c < len(lines):
+        line = lines[c].strip()
+        #print ':' + line + ':'
+
+        if line in ARG_COMMANDS:            
+            c+= 1
+            args = lines[c].strip().split(' ')
+            #print 'args\t' + str(args)
+
+        if line == 'pop':
+            if len(stack) > 1:
+                stack.pop()
+            else:
+                print 'cannot pop ident stack'
+
+        elif line == 'push':
+            stack.append(deepcopy(stack[-1]))                                                                                                              
+        elif line == 'sphere':
+            #print 'SPHERE\t' + str(args)
+            add_sphere(edges,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
+            new_coord()
+            
+        elif line == 'torus':
+            #print 'TORUS\t' + str(args)
+            add_torus(edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), step)
+            new_coord()
+        elif line == 'box':
+            print 'BOX\t' + str(args)
+            add_box(edges,
+                    float(args[0]), float(args[1]), float(args[2]),
+                    float(args[3]), float(args[4]), float(args[5]))
+            new_coord()
+        elif line == 'circle':
+            #print 'CIRCLE\t' + str(args)
+            add_circle(edges,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
+            new_coord(True)
+
+        elif line == 'hermite' or line == 'bezier':
+            #print 'curve\t' + line + ": " + str(args)
+            add_curve(edges,
+                      float(args[0]), float(args[1]),
+                      float(args[2]), float(args[3]),
+                      float(args[4]), float(args[5]),
+                      float(args[6]), float(args[7]),
+                      step, line)                      
+            new_coord(True)
+        elif line == 'line':            
+            #print 'LINE\t' + str(args)
+
+            add_edge( edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            new_coord(True)
+        elif line == 'scale':
+            #print 'SCALE\t' + str(args)
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(t, stack[-1])
+
+        elif line == 'move':
+            #print 'MOVE\t' + str(args)
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(t, stack[-1])
+
+        elif line == 'rotate':
+            #print 'ROTATE\t' + str(args)
+            theta = float(args[1]) * (math.pi / 180)
+            
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult(t, stack[-1])
+                
+        elif line == 'clear':
+            edges = []
+            
+        elif line == 'ident':
+            ident(transform)
+
+        elif line == 'apply':
+            matrix_mult( transform, edges )
+
+        elif line == 'display' or line == 'save':
+            #clear_screen(screen)
+            #draw_lines(edges, screen, color)
+            #draw_polygons(edges, screen, color)
+
+            if line == 'display':
+                print edges
+                display(screen)
+            else:
+                save_extension(screen, args[0])
+            
+        c+= 1
