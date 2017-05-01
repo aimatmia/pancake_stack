81c3247d2bc2294af7bdd682c09417b619799a50
Mia <mobrien@stuy.edu>
2017-05-01 16:58:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import math
 screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
+polygons = []
 transform = new_matrix()
 
 
-parse_file( 'script', edges, transform, screen, color )
+parse_file( 'script', edges, polygons, transform, screen, color )
