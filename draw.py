d92719aadc3423c9732894a33d2f7eba87ce2e29
Mia <mobrien@stuy.edu>
2017-05-21 18:45:58 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -137,163 +137,159 @@ def add_torus( edges, cx, cy, cz, r0, r1, step ):
                         p1[0], p1[1], p1[2])
 
 def generate_torus( cx, cy, cz, r0, r1, step ):
-    points = []
-    n_steps = int(1/step+0.1)
-    
-    rot_start = 0
-    rot_stop = n_steps
-    circ_start = 0
-    circ_stop = n_steps
-    
-    for rotation in range(rot_start, rot_stop):
-        rot = step * rotation
-        for circle in range(circ_start, circ_stop):
-            circ = step * circle
+    	points = []
+    	n_steps = int(1/step+0.1)
 
-            x = math.cos(2*math.pi*rot) * (r0 * math.cos(2*math.pi*circ) + r1) + cx;
-            y = r0 * math.sin(2*math.pi * circ) + cy;
-            z = -1*math.sin(2*math.pi*rot) * (r0 * math.cos(2*math.pi*circ) + r1) + cz;
-            points.append([x, y, z])          
-    return points
+    	rot_start = 0
+    	rot_stop = n_steps
+    	circ_start = 0
+    	circ_stop = n_steps
+    	
+	for rotation in range(rot_start, rot_stop):
+        	rot = step * rotation
+        for circle in range(circ_start, circ_stop):
+            	circ = step * circle
+
+            	x = math.cos(2*math.pi*rot) * (r0 * math.cos(2*math.pi*circ) + r1) + cx;
+            	y = r0 * math.sin(2*math.pi * circ) + cy;
+            	z = -1*math.sin(2*math.pi*rot) * (r0 * math.cos(2*math.pi*circ) + r1) + cz;
+            	points.append([x, y, z])          
+    	return points
 
 def add_circle( points, cx, cy, cz, r, step ):
-    x0 = r + cx
-    y0 = cy
-    t = step
+	x0 = r + cx
+    	y0 = cy
+    	t = step
 
-    while t <= 1.00001:
-        x1 = r * math.cos(2*math.pi * t) + cx;
-        y1 = r * math.sin(2*math.pi * t) + cy;
+    	while t <= 1.00001:
+        	x1 = r * math.cos(2*math.pi * t) + cx;
+        	y1 = r * math.sin(2*math.pi * t) + cy;
 
-        add_edge(points, x0, y0, cz, x1, y1, cz)
+        	add_edge(points, x0, y0, cz, x1, y1, cz)
         
-        x0 = x1
-        y0 = y1
-        t += step
+        	x0 = x1
+        	y0 = y1
+        	t += step
 
 def add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+	xcoefs = generate_curve_coefs(x0, x1, x2, x3, curve_type)[0]
+    	ycoefs = generate_curve_coefs(y0, y1, y2, y3, curve_type)[0]
 
-    xcoefs = generate_curve_coefs(x0, x1, x2, x3, curve_type)[0]
-    ycoefs = generate_curve_coefs(y0, y1, y2, y3, curve_type)[0]
-
-    t = step
-    while t <= 1.00001:
-        x = xcoefs[0] * t^3 + xcoefs[1] * t^2 + xcoefs[2] * t + xcoefs[3]
-        y = ycoefs[0] * t^3 + ycoefs[1] * t^2 + ycoefs[2] * t + ycoefs[3]
+    	t = step
+    	while t <= 1.00001:
+        	x = xcoefs[0] * t^3 + xcoefs[1] * t^2 + xcoefs[2] * t + xcoefs[3]
+        	y = ycoefs[0] * t^3 + ycoefs[1] * t^2 + ycoefs[2] * t + ycoefs[3]
                 
-        add_edge(points, x0, y0, 0, x, y, 0)
+		add_edge(points, x0, y0, 0, x, y, 0)
         
-        x0 = x
-        y0 = y
-        t += step
+        	x0 = x
+        	y0 = y
+        	t += step
 
 def draw_lines( points, screen, color ):
-    if len(points) < 2:
-        print 'Need at least 2 points to draw'
-        return
-    
-    i = 0
-    while i < len(points) - 1:
-        draw_line( int(points[i][0]),
-                   int(points[i][1]),
-                   int(points[i+1][0]),
-                   int(points[i+1][1]),
-                   screen, color)    
-        i+= 2
+	if len(points) < 2:
+        	print 'Need at least 2 points to draw'
+        	return
+    	
+	i = 0
+    	while i < len(points) - 1:
+        	draw_line( int(points[i][0]),
+                   	int(points[i][1]),
+                   	int(points[i+1][0]),
+                   	int(points[i+1][1]),
+                   	screen, color)    
+        	i+= 2
         
 def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
-    add_point(matrix, x0, y0, z0)
-    add_point(matrix, x1, y1, z1)
-    
+    	add_point(matrix, x0, y0, z0)
+    	add_point(matrix, x1, y1, z1)
+
 def add_point( matrix, x, y, z=0 ):
-    matrix.append( [x, y, z, 1] )
-    
-
-
+    	matrix.append( [x, y, z, 1] )
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    x0 = int(x0)
-    y0 = int(y0)
-    x1 = int(x1)
-    y1 = int(y1)
-    #swap points if going right -> left
-    if x0 > x1:
-        xt = x0
-        yt = y0
-        x0 = x1
-        y0 = y1
-        x1 = xt
-        y1 = yt
+    	x0 = int(x0)
+    	y0 = int(y0)
+    	x1 = int(x1)
+    	y1 = int(y1)
+    	#swap points if going right -> left
+    	if x0 > x1:
+        	xt = x0
+        	yt = y0
+        	x0 = x1
+        	y0 = y1
+        	x1 = xt
+        	y1 = yt
 
-    x = x0
-    y = y0
-    A = 2 * (y1 - y0)
-    B = -2 * (x1 - x0)
+    	x = x0
+    	y = y0
+    	A = 2 * (y1 - y0)
+    	B = -2 * (x1 - x0)
 
-    #octants 1 and 8
-    if ( abs(x1-x0) >= abs(y1 - y0) ):
+    	#octants 1 and 8
+    	if ( abs(x1-x0) >= abs(y1 - y0) ):
 
-        #octant 1
-        if A > 0:            
-            d = A + B/2
+        	#octant 1
+        	if A > 0:            
+            		d = A + B/2
 
-            while x < x1:
-                plot(screen, color, x, y)
-                if d > 0:
-                    y+= 1
-                    d+= B
-                x+= 1
-                d+= A
-            #end octant 1 while
-            plot(screen, color, x1, y1)
-        #end octant 1
+            		while x < x1:
+                		plot(screen, color, x, y)
+                		if d > 0:
+                    			y+= 1
+                    			d+= B
+				x+= 1
+                		d+= A
+            		#end octant 1 while
+            		plot(screen, color, x1, y1)
+        	#end octant 1
 
-        #octant 8
-        else:
-            d = A - B/2
+        	#octant 8
+        	else:
+            		d = A - B/2
 
-            while x < x1:
-                plot(screen, color, x, y)
-                if d < 0:
-                    y-= 1
-                    d-= B
-                x+= 1
-                d+= A
-            #end octant 8 while
-            plot(screen, color, x1, y1)
-        #end octant 8
-    #end octants 1 and 8
+            		while x < x1:
+                		plot(screen, color, x, y)
+                		if d < 0:
+                   			y-= 1
+                    			d-= B
+                		x+= 1
+                		d+= A
+            		#end octant 8 while
+            		plot(screen, color, x1, y1)
+        	#end octant 8
+	#end octants 1 and 8
 
-    #octants 2 and 7
-    else:
-        #octant 2
-        if A > 0:
-            d = A/2 + B
+    	#octants 2 and 7
+    	else:
+        	#octant 2
+        	if A > 0:
+            		d = A/2 + B
 
-            while y < y1:
-                plot(screen, color, x, y)
-                if d < 0:
-                    x+= 1
-                    d+= A
-                y+= 1
-                d+= B
-            #end octant 2 while
-            plot(screen, color, x1, y1)
-        #end octant 2
+            		while y < y1:
+                		plot(screen, color, x, y)
+                		if d < 0:
+                    			x+= 1
+                    			d+= A
+                		y+= 1
+                		d+= B
+            		#end octant 2 while
+            		plot(screen, color, x1, y1)
+        	#end octant 2
 
-        #octant 7
-        else:
-            d = A/2 - B;
+        	#octant 7
+        	else:
+			d = A/2 - B;
 
-            while y > y1:
-                plot(screen, color, x, y)
-                if d > 0:
-                    x+= 1
-                    d+= A
-                y-= 1
-                d-= B
-            #end octant 7 while
-            plot(screen, color, x1, y1)
-        #end octant 7
-    #end octants 2 and 7
+            		while y > y1:
+                		plot(screen, color, x, y)
+                		if d > 0:
+                    			x+= 1
+                    			d+= A
+                		y-= 1
+                		d-= B
+            		#end octant 7 while
+            		plot(screen, color, x1, y1)
+        	#end octant 7
+    	#end octants 2 and 7
 #end draw_line
