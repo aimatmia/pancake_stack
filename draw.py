7b9880ba85f2a476799c657336aa2ee8343f4c4e
Mia <mobrien@stuy.edu>
2017-05-21 17:40:39 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -18,11 +18,11 @@ def draw_polygons( points, screen, color ):
         B = [0, 0, 0]
         prod = [0, 0, 0]
     
-        A[0] = polygons[i+1][0] - polygons[i][0]
-        A[1] = polygons[i+1][1] - polygons[i][1]
+        A[0] = points[i+1][0] - points[i][0]
+        A[1] = points[i+1][1] - points[i][1]
 
-        B[0] = polygons[i+2][0] - polygons[i][0]
-        B[1] = polygons[i+2][1] - polygons[i][1]
+        B[0] = points[i+2][0] - points[i][0]
+        B[1] = points[i+2][1] - points[i][1]
 
         prod[2] = A[0] * B[1] - A[1] * B[0]
 
